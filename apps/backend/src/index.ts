6b804729c6999100fd52fe6e54b8cba178e50a7c
Abdul Aliyev <aaa7c4@gmail.com>
2026-01-12 22:30:56 -0500

    Try fix cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -19,6 +19,8 @@ const allowedOrigins =
         'http://localhost:5173',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
+        'https://aaliyev.com',
+        'https://www.aaliyev.com',
       ]
     : config.ALLOWED_ORIGINS;
 
